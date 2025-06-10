a803176d966a4d508a82f37378fa037b460597a8
bbFIGHTON <bbfighton@gmail.com>
2025-06-10 03:44:56 -0700

    Count Occurrences

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -27,6 +27,15 @@ def insert_items(s, before, after):
     True
     """
     "*** YOUR CODE HERE ***"
+    i = 0
+    while i < len(s):
+        if s[i] == before:
+            s.insert(i+1, after)
+            i += 2
+        else:
+            i += 1
+    return s
+
 
 
 def group_by(s, fn):
@@ -40,12 +49,12 @@ def group_by(s, fn):
     {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
     """
     grouped = {}
-    for ____ in ____:
-        key = ____
+    for e in s:
+        key = fn(e)
         if key in grouped:
-            ____
+            grouped[key].append(e)
         else:
-            grouped[key] = ____
+            grouped[key] = [e]
     return grouped
 
 
@@ -71,7 +80,11 @@ def count_occurrences(t, n, x):
     2
     """
     "*** YOUR CODE HERE ***"
-
+    count = 0
+    for _ in range(n):
+        if x == next(t):
+            count += 1
+    return count
 
 def repeated(t, k):
     """Return the first value in iterator t that appears k times in a row,
