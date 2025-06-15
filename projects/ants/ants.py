128d7018dd41f4b601836a9238a7a0db5541d57e
bbFIGHTON <bbfighton@gmail.com>
2025-06-15 02:32:46 -0700

    class ContainerAnt

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -329,16 +329,21 @@ class ContainerAnt(Ant):
 
     def __init__(self, health):
         super().__init__(health)
-        self.ant_contained = None
+        self.ant_contained = None # stores the ant it contains
 
     def can_contain(self, other):
         # BEGIN Problem 8a
         "*** YOUR CODE HERE ***"
+        if self.ant_contained == None and other.is_container == False:
+            return True
+        else:
+            return False
         # END Problem 8a
 
     def store_ant(self, ant):
         # BEGIN Problem 8a
         "*** YOUR CODE HERE ***"
+        self.ant_contained = ant
         # END Problem 8a
 
     def remove_ant(self, ant):
@@ -359,7 +364,11 @@ class ContainerAnt(Ant):
     def action(self, gamestate):
         # BEGIN Problem 8a
         "*** YOUR CODE HERE ***"
+        if self.ant_contained:
+            self.ant_contained.action(gamestate)
         # END Problem 8a
+
+        
 
 
 class BodyguardAnt(ContainerAnt):
