a72aba140d27912a68721ddd1780582009496f91
bbFIGHTON <bbfighton@gmail.com>
2025-06-28 02:33:26 -0700

    define (interleave lst1 lst2)

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -6,8 +6,14 @@
             #f)) ;  (and (<= (car s) (car (cdr s))) (ascending? (cdr s))))
 )
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    )
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
+;       (else (cons (car lst1) (interleave lst2 (cdr lst1))))
 
 (define (no-repeats s) 'YOUR-CODE-HERE)
